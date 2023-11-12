0f9a657714f17326614d07deed073a65cedb36cc
ali99944 <ali99944>
2023-11-12 23:53:31 +0200

    upload

--- utils/mongodbConnection.js
+++ utils/mongodbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-// const uri = 'mongodb://admin:admin123@127.0.0.1:27017/admin'; // Replace with your MongoDB connection URI
-const uri = 'mongodb+srv://danaaka8:p6TPp7ILrQUlDKfE@cluster0.uyhqfof.mongodb.net/nordic'; // Replace with your MongoDB connection URI
+const uri = 'mongodb://admin:admin123@127.0.0.1:27017/admin'; // Replace with your MongoDB connection URI
+// const uri = 'mongodb+srv://danaaka8:p6TPp7ILrQUlDKfE@cluster0.uyhqfof.mongodb.net/nordic'; // Replace with your MongoDB connection URI
 
 
 mongoose.connect(uri).then(() => console.log('connected mongoose'))
